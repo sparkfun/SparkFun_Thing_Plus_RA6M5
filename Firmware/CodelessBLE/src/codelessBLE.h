0618310ba6e5914a1d1f41dbe3d49bccc4a10ee7
SFE-Brudnerd <101155592+SFE-Brudnerd@users.noreply.github.com>
2024-03-19 13:31:03 -0600

    Remove WIP binary mode code.

--- Firmware/CodelessBLE/src/codelessBLE.h
+++ Firmware/CodelessBLE/src/codelessBLE.h
@@ -6,8 +6,6 @@
 
 // Uncomment if you want to enable debug messages
 // #define CODELESS_DBG
-// Uncomment if you want to use the codeless data pump binary. CURRENTLY UNSUPPORTED.
-// #define CODELESS_DATA_PUMP
 
 #define DEFAULT_CODELESS_BAUD_RATE 57600
 #define DEFAULT_TIMEOUT_IN_S 2
@@ -40,16 +38,6 @@ public:
      * @brief Constructs a CodelessBase object.
      * @param serial The UART object to use for communication.
      */
-    #ifdef CODELESS_DATA_PUMP
-    CodelessBase(UART& serial = Serial2) : _serialPort{&serial},
-                                            _connected{false},
-                                            _reply_received{false},
-                                            _binary_mode{false},
-                                            _status{CODELESS_ERROR},
-                                            _reply_string{""}
-    {
-    }
-    #else
     CodelessBase(UART& serial = Serial2) : _serialPort{&serial},
                                             _connected{false},
                                             _reply_received{false},
@@ -57,7 +45,6 @@ public:
                                             _reply_string{""}
     {
     }
-    #endif
 
     /**
      * @brief Initializes the CodelessBase object.
@@ -85,16 +72,6 @@ public:
      * @return The output string from the Codeless device.
      */
     virtual String readOutput(void);
-
-    #ifdef CODELESS_DATA_PUMP
-    // bool enterBinaryMode(void);
-    // bool exitBinaryMode(void);
-
-    // bool sendEscapeSequence(void);
-    // bool sendBinaryResume(void);
-
-    // bool binaryMode(void);
-    #endif
 
     /**
      * @brief Checks if the Codeless device is connected.
@@ -127,26 +104,4 @@ protected:
     String _reply_string;
 
     UART* _serialPort;
-
-    #ifdef CODELESS_DATA_PUMP
-    bool _binary_mode;
-    // const String[12] unsolicited_str = {"\r\n+READY\r\n",
-    //                                     "\r\n+AWAKE\r\n",
-    //                                     "\r\n+CONNECTED\r\n",
-    //                                     "\r\n+DISCONNECTED\r\n",
-    //                                     "\r\n+BONDING SUCCESS\r\n",
-    //                                     "\r\n+PASSCODE REQ RECEIVED\r\n",
-    //                                     "\r\n+SECURITY REQ. AUTH LEVEL: ",
-    //                                     "\r\n+BINARY MODE SUPPORTED\r\n",
-    //                                     "\r\n+COMMAND MODE SUPPORTED\r\n",
-    //                                     "\r\n+ECHO ON\r\n",
-    //                                     "\r\n+ECHO OFF\r\n",
-    //                                     "\r\n+ESC SEQ\r\n"};
-
-    // const String[4] bin_unsolicited_str = {"\r\n+BINREQ\r\n\r\n",
-    //                                        "\r\n+BINREQACK\r\n\r\n",
-    //                                        "\r\n+BINREQEXITACK\r\n\r\n",
-    //                                        "\r\n+BINREQEXIT\r\n\r\n"};
-    #endif
-
 };
